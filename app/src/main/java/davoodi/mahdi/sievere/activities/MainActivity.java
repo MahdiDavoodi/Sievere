69d2bbb7f80787d03cce8132e2eb84708d97a42b
mahdidavoodi <mahdidavoodi@gmail.com>
2021-06-08 19:06:45 +0300

    Initial commit

---
+++ app/src/main/java/davoodi/mahdi/sievere/activities/MainActivity.java
@@ -0,0 +1,16 @@
+package davoodi.mahdi.sievere.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import davoodi.mahdi.sievere.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
