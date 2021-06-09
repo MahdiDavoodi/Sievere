f1d06562599062d3589d12ce344600631fec3ac1
mahdidavoodi <mahdidavoodi@gmail.com>
2021-06-09 15:21:17 +0300

    Finish IntroActivity for now!

--- app/src/main/java/davoodi/mahdi/sievere/activities/IntroActivity.java
+++ app/src/main/java/davoodi/mahdi/sievere/activities/IntroActivity.java
@@ -2,15 +2,29 @@ package davoodi.mahdi.sievere.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import davoodi.mahdi.sievere.R;
 
 public class IntroActivity extends AppCompatActivity {
 
+    protected static final int DELAY = 800;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+        new Thread(this::nextActivity).start();
+    }
+
+    private void nextActivity() {
+        try {
+            Thread.sleep(DELAY);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            startActivity(new Intent(IntroActivity.this, MainActivity.class));
+        }
     }
 }
