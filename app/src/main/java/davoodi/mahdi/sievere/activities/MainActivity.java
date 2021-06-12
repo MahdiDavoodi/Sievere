77f0026ff2c25d6bc0c9f16d4b01323337d4693d
mahdidavoodi <mahdidavoodi@gmail.com>
2021-06-12 14:55:58 +0300

    Finally some good work! NavController woks fine and MainActivity.java is starting to be good.

--- app/src/main/java/davoodi/mahdi/sievere/activities/MainActivity.java
+++ app/src/main/java/davoodi/mahdi/sievere/activities/MainActivity.java
@@ -1,8 +1,15 @@
 package davoodi.mahdi.sievere.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import davoodi.mahdi.sievere.R;
 
@@ -12,5 +19,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.ma_nav_buttons);
+
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.ma_nav_host);
+        assert navHostFragment != null;
+        NavController navController = navHostFragment.getNavController();
+        /*
+        ****** In case we decide to shoe action bar in later versions. ******
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.tracksFragment, R.id.playlistsFragment, R.id.forYouFragment)
+                .build();
+
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);*/
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
     }
 }
