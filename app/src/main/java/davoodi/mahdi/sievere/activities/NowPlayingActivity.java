cd91f43ffb1fea0b13b4a81fdc190e301ec99a0a
mahdidavoodi <mahdidavoodi@gmail.com>
2021-07-04 15:59:55 +0300

    Edited the seekbar design.

--- app/src/main/java/davoodi/mahdi/sievere/activities/NowPlayingActivity.java
+++ app/src/main/java/davoodi/mahdi/sievere/activities/NowPlayingActivity.java
@@ -6,7 +6,6 @@ import androidx.core.content.ContextCompat;
 import android.os.Bundle;
 
 import com.masoudss.lib.WaveformSeekBar;
-import com.masoudss.lib.utils.Utils;
 import com.masoudss.lib.utils.WaveGravity;
 
 import davoodi.mahdi.sievere.R;
@@ -18,13 +17,8 @@ public class NowPlayingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_now_playing);
 
-        WaveformSeekBar waveformSeekBar = findViewById(R.id.waveformSeekBar);
-        waveformSeekBar.setProgress(33);
-        waveformSeekBar.setMaxProgress(100);
-        waveformSeekBar.setWaveGravity(WaveGravity.CENTER);
-        waveformSeekBar.setWaveBackgroundColor(ContextCompat.getColor(this, R.color.dark_secondary_text));
-        waveformSeekBar.setWaveProgressColor(ContextCompat.getColor(this, R.color.primary));
-        int[] array = {0, 1, 2, 3, 4, 5, 6, 2, 3, 8, 2, 3, 4, 6, 0, 8, 4, 6, 3, 2, 3, 5, 8, 0, 0, 0, 2, 5, 3, 6, 6, 4, 7, 1, 0, 0, 0};
+        WaveformSeekBar waveformSeekBar = findViewById(R.id.npa_seekbar);
+        int[] array = {0, 1, 2, 3, 4, 5, 6, 2, 3, 8, 2, 3, 4, 6, 1, 2, 3, 4, 5, 6, 2, 3, 8, 2, 3, 4, 6, 1, 2, 3, 4, 5, 6, 2, 3, 8, 2, 3, 4, 6, 1, 2, 3, 4, 5, 6, 2, 3, 8, 2, 3, 4, 6, 1, 2, 3, 4, 5, 6, 2, 3, 8, 2, 3, 4, 6, 1, 2, 3, 4, 5, 6, 2, 3, 8, 2, 3, 4, 6, 1, 2, 3, 4, 5, 6, 2, 3, 8, 2, 3, 4, 6, 1, 2, 3, 4, 5, 6, 2, 3, 8, 2, 3, 4, 6, 1, 2, 3, 4, 5, 6, 2, 3, 8, 2, 3, 4, 6, 0, 8, 4, 6, 3, 2, 3, 5, 8, 0, 0, 0, 2, 5, 3, 6, 6, 4, 7, 1, 0, 0, 0};
         waveformSeekBar.setSampleFrom(array);
     }
 }
