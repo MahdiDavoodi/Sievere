f0e94950ca92e784d379d115808da79e06076c98
mahdidavoodi <mahdidavoodi@gmail.com>
2021-07-04 17:59:23 +0300

    Bytes way does not work either.

--- app/src/main/java/davoodi/mahdi/sievere/activities/NowPlayingActivity.java
+++ app/src/main/java/davoodi/mahdi/sievere/activities/NowPlayingActivity.java
@@ -11,7 +11,15 @@ import com.masoudss.lib.WaveformSeekBar;
 import com.masoudss.lib.exception.AmplitudaNotFoundException;
 import com.masoudss.lib.utils.WaveGravity;
 
+import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.IntBuffer;
 
 import davoodi.mahdi.sievere.R;
 import davoodi.mahdi.sievere.data.DataLoader;
@@ -27,22 +35,28 @@ public class NowPlayingActivity extends AppCompatActivity {
         int[] array = {0, 1, 2, 3, 4, 5, 6, 2, 3, 8, 2, 3, 4, 6, 1, 2, 3, 4, 5, 6, 2, 3, 8, 2, 3, 4, 6, 1, 2, 3, 4, 5, 6, 2, 3, 8, 2, 3, 4, 6, 1, 2, 3, 4, 5, 6, 2, 3, 8, 2, 3, 4, 6, 1, 2, 3, 4, 5, 6, 2, 3, 8, 2, 3, 4, 6, 1, 2, 3, 4, 5, 6, 2, 3, 8, 2, 3, 4, 6, 1, 2, 3, 4, 5, 6, 2, 3, 8, 2, 3, 4, 6, 1, 2, 3, 4, 5, 6, 2, 3, 8, 2, 3, 4, 6, 1, 2, 3, 4, 5, 6, 2, 3, 8, 2, 3, 4, 6, 0, 8, 4, 6, 3, 2, 3, 5, 8, 0, 0, 0, 2, 5, 3, 6, 6, 4, 7, 1, 0, 0, 0};
         new Thread(() -> this.test(waveformSeekBar)).start();
         //waveformSeekBar.setSampleFrom(array);
-        try {
-            waveformSeekBar.setSampleFrom(new File(DataLoader.tracks.get(0).getUri().getPath()));
-            Log.i("MM", "LL" + DataLoader.tracks.get(0).getUri().getPath());
-        } catch (AmplitudaNotFoundException e) {
-            e.printStackTrace();
-            Log.i("MM", "LL");
-        }
     }
 
     private void test(WaveformSeekBar waveformSeekBar) {
+        File file = new File(DataLoader.tracks.get(0).getUri().getPath());
+        int size = (int) file.length();
+        byte[] bytes = new byte[size];
         try {
-            waveformSeekBar.setSampleFrom(new File(DataLoader.tracks.get(0).getUri().getPath()));
-            Log.i("MM", "LL" + DataLoader.tracks.get(0).getUri().getPath());
-        } catch (AmplitudaNotFoundException e) {
+            BufferedInputStream buf = new BufferedInputStream(new FileInputStream(file));
+            buf.read(bytes, 0, bytes.length);
+            buf.close();
+        } catch (IOException e) {
             e.printStackTrace();
-            Log.i("MM", "LL");
         }
+
+        IntBuffer intBuf =
+                ByteBuffer.wrap(bytes)
+                        .order(ByteOrder.BIG_ENDIAN)
+                        .asIntBuffer();
+        int[] array = new int[intBuf.remaining()];
+        intBuf.get(array);
+
+            waveformSeekBar.setSampleFrom(array);
+            Log.i("MM", "LL" + DataLoader.tracks.get(0).getUri().getPath());
     }
 }
