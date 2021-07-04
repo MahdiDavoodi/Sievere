c50c179ac9100e652524f6bd60380117a00ebe54
mahdidavoodi <mahdidavoodi@gmail.com>
2021-07-04 16:55:17 +0300

    Working on seekbar. reading from audio.

--- app/src/main/java/davoodi/mahdi/sievere/activities/NowPlayingActivity.java
+++ app/src/main/java/davoodi/mahdi/sievere/activities/NowPlayingActivity.java
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 
 import com.masoudss.lib.WaveformSeekBar;
+import com.masoudss.lib.exception.AmplitudaNotFoundException;
 import com.masoudss.lib.utils.WaveGravity;
 
+import java.io.File;
+
 import davoodi.mahdi.sievere.R;
+import davoodi.mahdi.sievere.data.DataLoader;
 
 public class NowPlayingActivity extends AppCompatActivity {
 
@@ -19,6 +25,17 @@ public class NowPlayingActivity extends AppCompatActivity {
 
         WaveformSeekBar waveformSeekBar = findViewById(R.id.npa_seekbar);
         int[] array = {0, 1, 2, 3, 4, 5, 6, 2, 3, 8, 2, 3, 4, 6, 1, 2, 3, 4, 5, 6, 2, 3, 8, 2, 3, 4, 6, 1, 2, 3, 4, 5, 6, 2, 3, 8, 2, 3, 4, 6, 1, 2, 3, 4, 5, 6, 2, 3, 8, 2, 3, 4, 6, 1, 2, 3, 4, 5, 6, 2, 3, 8, 2, 3, 4, 6, 1, 2, 3, 4, 5, 6, 2, 3, 8, 2, 3, 4, 6, 1, 2, 3, 4, 5, 6, 2, 3, 8, 2, 3, 4, 6, 1, 2, 3, 4, 5, 6, 2, 3, 8, 2, 3, 4, 6, 1, 2, 3, 4, 5, 6, 2, 3, 8, 2, 3, 4, 6, 0, 8, 4, 6, 3, 2, 3, 5, 8, 0, 0, 0, 2, 5, 3, 6, 6, 4, 7, 1, 0, 0, 0};
-        waveformSeekBar.setSampleFrom(array);
+        new Thread(() -> this.test(waveformSeekBar)).start();
+        //waveformSeekBar.setSampleFrom(array);
+    }
+
+    private void test(WaveformSeekBar waveformSeekBar) {
+        try {
+            waveformSeekBar.setSampleFrom(new File(DataLoader.tracks.get(0).getUri().getPath()));
+            Log.i("MM", "LL" + DataLoader.tracks.get(0).getUri().getPath());
+        } catch (AmplitudaNotFoundException e) {
+            e.printStackTrace();
+            Log.i("MM", "LL");
+        }
     }
 }
