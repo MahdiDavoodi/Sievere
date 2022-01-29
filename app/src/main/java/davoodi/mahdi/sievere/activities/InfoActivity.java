8f2e9b68ca6a2a1c2c18b3e42f80187d54b3d94e
MahdiDavoodi <MahdiDavoodi@gmail.com>
2022-01-29 11:46:30 -0500

    Update - Info Activity
    ---
    Removed Telegram button from Info Activity.

--- app/src/main/java/davoodi/mahdi/sievere/activities/InfoActivity.java
+++ app/src/main/java/davoodi/mahdi/sievere/activities/InfoActivity.java
@@ -11,17 +11,12 @@ import davoodi.mahdi.sievere.R;
 
 public class InfoActivity extends AppCompatActivity {
 
-    public static final String TELEGRAM = "https://t.me/Sievere";
     public static final String REPOSITORY = "https://github.com/MahdiDavoodi/Sievere";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
-    }
-
-    public void infoTelegram(View view) {
-        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(TELEGRAM)));
     }
 
     public void infoGithub(View view) {
